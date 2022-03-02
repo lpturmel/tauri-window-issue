21b5b11cbde53d266084b00b9976d58098e4f702
Louis-Philippe Turmel <louisphilippe.turmel@gmail.com>
2022-03-02 15:07:08 -0800

    added setup function

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -6,11 +6,18 @@ use url::Url;
 
 fn main() {
   tauri::Builder::default()
+  .setup(|app| {
+    let external_url = Url::parse("https://google.com").unwrap();
+
+    app.create_window("window_from_main".to_string(), tauri::WindowUrl::External(external_url), move |window_builder, webview_attributes| {
+        (window_builder, webview_attributes)
+    });
+    Ok(())
+  })
   .invoke_handler(tauri::generate_handler![
     spawn_window
   ])
     .run(tauri::generate_context!())
-    
     .expect("error while running tauri application");
 }
 
@@ -18,7 +25,7 @@ fn main() {
 fn spawn_window(app: tauri::AppHandle) {
   let external_url = Url::parse("https://google.com").unwrap();
 
-  app.create_window("discord".to_string(), tauri::WindowUrl::External(external_url), move |window_builder, webview_attributes| {
+  app.create_window("command_window".to_string(), tauri::WindowUrl::External(external_url), move |window_builder, webview_attributes| {
       (window_builder, webview_attributes)
   });
     
